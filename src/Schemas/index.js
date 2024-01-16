1f68e22ad0b3eabc557ae29b1b147f967e2697ec
Marwin <yuthmarwin@gmail.com>
2024-01-16 08:47:25 +0700

    commit

--- src/Schemas/index.js
+++ src/Schemas/index.js
@@ -1,9 +1,7 @@
-import * as yup from "yup"
+import * as yup from 'yup'
 
 
-export const basicSchema = yup.object().shape({
-    email:yup.string().email("Please enter a valid email").required('Required'),
-    age:yup.number().positive().integer().required('Required'),
-    password:yup.string().min(5).max(20).required('Required'),
-    confirm:yup.string().oneOf([yup.ref('password'),"Password must match"]).required('Required')  
+export const categorySchema = yup.object().shape({
+    name:yup.string().required('required'),
+    code:yup.number().positive().required('required')
 })
